c69fa66ee31658a8b9c3466bf8149954ef961ecb
bignick316 <91993659+bignick316@users.noreply.github.com>
2022-03-25 22:06:50 -0500

    Add files via upload

---
+++ Midterm/view/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="container-fluid flex-grow-1 ">
+        <div class="row py-5">
+            <div class="col-lg-12 text-white text-center">
+                <p>
+                    &copy; <?php echo date("Y"); ?> Zippy's used auto cars.
+                </p>
+            </div> 
+        </div>
+    </div>
+</footer>
+</body>
+</html>
