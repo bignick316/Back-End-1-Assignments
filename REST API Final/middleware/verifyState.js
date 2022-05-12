b8e9fc7ec641d6691e37c85133a123a453480345
bignick316 <91993659+bignick316@users.noreply.github.com>
2022-05-12 11:17:42 -0500

    Create verifyState.js

---
+++ REST API Final/middleware/verifyState.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const statesData = require('../config/statesData');
+
+
+const verifyState = async (req, res,  next) => {
+    var data = statesData();
+    var stateCodes = [];
+
+    for (var i = 0; i < data.length; i++)
+    {
+        stateCodes.push(data[i].code);
+    }
+    if (stateCodes.indexOf(req.params.state.toUpperCase()) == -1)
+    {
+        res.status(404);
+        res.json({"message": "Invalid state abbreviation"});
+    }
+    else
+    {
+        next();
+    }
+}
+
+module.exports = {verifyState};
